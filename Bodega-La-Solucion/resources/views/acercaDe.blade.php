157012e31aa9a422ad00c6051d8e14903931905a
JenniferGFugon <jguerrerofugon500@gmail.com>
2021-03-14 12:05:58 -0600

    arreglar formulario de contactanos

--- Bodega-La-Solucion/resources/views/acercaDe.blade.php
+++ Bodega-La-Solucion/resources/views/acercaDe.blade.php
@@ -29,44 +29,49 @@
             </p>
         </div>
     </div>
-    <div class="container">
-        <div class="row">
+
+    <div class="outer-container">
+
+    <div class="about-section">
+    </div>
+    <div class="inner-container">
+
             <div class="col-md-12">
                 <div class="well well-sm">
-                    <form class="form-horizontal" method="post">
+                    <form class="form-vertical" method="post">
                         <fieldset>
                             <legend class="text-center header">Contactanos</legend>
     
                             <div class="form-group">
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <input id="fname" name="name" type="text" placeholder="nombre" class="form-control">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-user bigicon"></i></span>
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <input id="lname" name="name" type="text" placeholder="Apellido" class="form-control">
                                 </div>
                             </div>
     
                             <div class="form-group">
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <input id="email" name="email" type="text" placeholder="Email " class="form-control">
                                 </div>
                             </div>
     
                             <div class="form-group">
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-phone-square bigicon"></i></span>
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <input id="phone" name="phone" type="text" placeholder="Telefono" class="form-control">
                                 </div>
                             </div>
     
                             <div class="form-group">
                                 <span class="col-md-1 col-md-offset-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
-                                <div class="col-md-8">
+                                <div class="col-md-12">
                                     <textarea class="form-control" id="message" name="message" placeholder="Escribe tu mensaje aqui" rows="7"></textarea>
                                 </div>
                             </div>
@@ -79,7 +84,9 @@
                         </fieldset>
                     </form>
                 </div>
-            </div>
-        </div>
+                                </div>
+
+                            </div>
     </div>
+</div>
 @endsection
